932a8cb608be05289fb29ca43318f17a15691d69
Marco Tello <marcotello5@outlook.com>
2025-10-31 04:06:40 +0000

    Adding the project structure, the main layout and the landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './layout/main/main-layout/main-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/landing',
+        pathMatch: 'full'
+    },
+    {
+        path: '',
+        component: MainLayout,
+        children: [
+          {
+            path: 'landing',
+            loadComponent: () => import('./features/landing/landing').then(m => m.Landing)
+          }
+        ]
+      },
+      {
+        path: '**',
+        redirectTo: '/landing'
+      }
+];
---
+++ src/app/features/landing/landing.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  imports: [],
+  templateUrl: './landing.html',
+  styleUrl: './landing.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Landing {
+
+}
