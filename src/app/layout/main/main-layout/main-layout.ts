40f8329287a218892931add7bfa0fc12cc2a0aab
Marco Tello <marcotello5@outlook.com>
2025-10-31 04:09:17 +0000

    Fixing bugs in the Main Layout, in the Header and in the footer

---
+++ src/app/layout/main/main-layout/main-layout.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from "../header/header";
+import { Footer } from "../footer/footer";
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [RouterOutlet, Header, Footer],
+  templateUrl: './main-layout.html',
+  styleUrl: './main-layout.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MainLayout {
+
+}
