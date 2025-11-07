b1ecf25384c3f18faee319df638dbd0ac9b2ccd6
Marco Tello <marcotello5@outlook.com>
2025-11-06 19:34:15 -0600

    002-three-column-footer Adding the footer with styles

--- src/app/layout/main/footer/footer.ts
+++ src/app/layout/main/footer/footer.ts
@@ -1,12 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Footer {
-
+  // Image source constants
+  readonly logoSrc = '/images/saga_logo_bottom.png';
+  readonly illustrationSrc = '/images/reading2.svg';
+  
+  // Alt text constants
+  readonly logoAlt = 'Saga';
+  readonly illustrationAlt = 'Saga - Book Tracker';
+  
+  // GitHub repository URL
+  readonly githubUrl = 'https://github.com/marcotello/saga';
 }
