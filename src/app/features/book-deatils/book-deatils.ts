36b0f606fb10ff7d89d37976e9e4e78937916a06
Marco Tello <marcotello5@outlook.com>
2026-03-09 00:37:04 -0600

    Adding the book details initial page

---
+++ src/app/features/book-deatils/book-deatils.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-deatils',
+  imports: [],
+  templateUrl: './book-deatils.html',
+  styleUrl: './book-deatils.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BookDeatils {
+
+}
